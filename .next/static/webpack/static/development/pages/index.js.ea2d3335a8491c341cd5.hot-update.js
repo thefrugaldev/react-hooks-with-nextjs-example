18dbc0393ceb5f82ab21fb14e9f46a945fc5b648
Orman, Clayton <CDO8081@yum.com>
2019-10-04 12:53:35 -0400

    Add initial index page

---
+++ .next/static/webpack/static/development/pages/index.js.ea2d3335a8491c341cd5.hot-update.js
@@ -0,0 +1,34 @@
+webpackHotUpdate("static\\development\\pages\\index.js",{
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "C:\\Projects\\react-hooks-with-nextjs-example\\pages\\index.js";
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+
+var InputElement = function InputElement() {
+  return __jsx("input", {
+    placeholder: "Enter Some Text",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4
+    },
+    __self: this
+  });
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (InputElement);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.ea2d3335a8491c341cd5.hot-update.js.map
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+
+const InputElement = () => {
+  return <input onChange={e => {}} placeholder="Enter Some Text" />;
+};
+
+export default InputElement;
