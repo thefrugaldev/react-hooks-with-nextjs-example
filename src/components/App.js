f483ceb35f6f023cc483135e59b080ec6a982685
Orman, Clayton <CDO8081@yum.com>
2019-10-18 10:54:29 -0400

    Add context api to project

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,24 @@ import React from "react";
 import Home from "./Home";
 import Whiskeys from "./Whiskeys";
 
+export const ConfigContext = React.createContext(); //Creates context for the entire App
+
 const pageToShow = pageName => {
   if (pageName === "Home") return <Home />;
   if (pageName === "Whiskeys") return <Whiskeys />;
   return <div>Not Found :(</div>;
 };
 
+const configValue = {
+  showBourbonType: true
+};
+
 const App = ({ pageName }) => {
-  return <div>{pageToShow(pageName)}</div>;
+  return (
+    <ConfigContext.Provider value={configValue}>
+      <div>{pageToShow(pageName)}</div>
+    </ConfigContext.Provider>
+  );
 };
 
 export default App;
